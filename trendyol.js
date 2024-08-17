dba0a03739b1e6d537c3ebb961ef83925fdf3ee6
mehmetA58 <87230646+mehmetA58@users.noreply.github.com>
2024-08-17 13:07:50 +0300

    Add files via upload

---
+++ trendyol.js
@@ -0,0 +1,14 @@
+output.trendyol = {
+    anasayfaButton: 'trendyol.com:id/navigation_bar_item_large_label_view',
+    hesabimButton: 'trendyol.com:id/tab_account',
+
+    // Hizmetlerimizi Keşfet Ekranı
+    hizmetlerimiziKesfet: {
+        title: 'trendyol.com:id/textViewTitle',
+        firstcardImage: 'trendyol.com:id/imageSliderBannerItem'
+        },
+    //Popüler Ürünler Ekranı 
+    populerUrunler: {
+        tumunuGorButton: 'trendyol.com:id/textViewNavigationTitle'
+        }   
+}
